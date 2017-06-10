377047bbad90b0a7e9427245f5c4aa4d4291a900
Zach Morse <zacharymorse@gmail.com>
2017-06-10 13:01:58 -0500

    begin work on javascript

---
+++ main.js
@@ -0,0 +1,11 @@
+//
+// This program will:
+// 1. take button input from keypresses
+// 2. display each keypress in display panel
+// 3. convert those keypresses to string
+// 4. append each new keypress to a string
+// 5. store that string, and allow appending
+// 6. when equals button is pressed, solve equation
+// 7. equation solved by eval(string)
+// 8. output of eval goes to display area
+// 9. clr button clears string and display area
