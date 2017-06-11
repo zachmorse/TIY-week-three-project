2b932cc66ea8d881208e342b44841164cf5647ea
Zach Morse <zacharymorse@gmail.com>
2017-06-11 16:09:11 -0500

    begin work on javascript, event listeners and basic functions

--- main.js
+++ main.js
@@ -1,11 +1,125 @@
 //
 // This program will:
-// 1. take button input from keypresses
+// 1. listen for keypresses of each element
 // 2. display each keypress in display panel
-// 3. convert those keypresses to string
-// 4. append each new keypress to a string
-// 5. store that string, and allow appending
-// 6. when equals button is pressed, solve equation
-// 7. equation solved by eval(string)
-// 8. output of eval goes to display area
-// 9. clr button clears string and display area
+// 3. functions will append element to array when pressed
+// 4. array is appended with each keypress
+// 5. array elements will be joined together and made into string
+// 6. string will be solved as equation by eval when "equals" is pressed
+// 7. "C" button will convert array to empty array
+
+// Declaring initial global variables:
+var calcArray = [];
+
+var button0 = document.getElementById("button-zero");
+var button1 = document.getElementById("button-one");
+var button2 = document.getElementById("button-two");
+var button3 = document.getElementById("button-three");
+var button4 = document.getElementById("button-four");
+var button5 = document.getElementById("button-five");
+var button6 = document.getElementById("button-six");
+var button7 = document.getElementById("button-seven");
+var button8 = document.getElementById("button-eight");
+var button9 = document.getElementById("button-nine");
+
+var buttonAdd = document.getElementById("button-add");
+var buttonSubtract = document.getElementById("button-minus");
+var buttonDivide = document.getElementById("button-divide");
+var buttonMultiply = document.getElementById("button-multiply");
+var buttonDecimal = document.getElementById("button-decimal");
+
+// adding event listeners for each clickable element:
+
+button0.addEventListener("click", buttonZero);
+button1.addEventListener("click", buttonOne);
+button2.addEventListener("click", buttonTwo);
+button3.addEventListener("click", buttonThree);
+button4.addEventListener("click", buttonFour);
+button5.addEventListener("click", buttonFive);
+button6.addEventListener("click", buttonSix);
+button7.addEventListener("click", buttonSeven);
+button8.addEventListener("click", buttonEight);
+button9.addEventListener("click", buttonNine);
+
+buttonAdd.addEventListener("click", buttonPlus);
+buttonSubtract.addEventListener("click", buttonMinus);
+buttonDivide.addEventListener("click", buttonDividedBy);
+buttonMultiply.addEventListener("click", buttonMultiplication);
+buttonDecimal.addEventListener("click", buttonDecimalPoint);
+
+// function calls for each button:
+
+function buttonZero() {
+  calcArray.push(0);
+  console.log(calcArray);
+}
+
+function buttonOne() {
+  calcArray.push(1);
+  console.log(calcArray);
+}
+
+function buttonTwo() {
+  calcArray.push(2);
+  console.log(calcArray);
+}
+
+function buttonThree() {
+  calcArray.push(3);
+  console.log(calcArray);
+}
+
+function buttonFour() {
+  calcArray.push(4);
+  console.log(calcArray);
+}
+
+function buttonFive() {
+  calcArray.push(5);
+  console.log(calcArray);
+}
+
+function buttonSix() {
+  calcArray.push(6);
+  console.log(calcArray);
+}
+
+function buttonSeven() {
+  calcArray.push(7);
+  console.log(calcArray);
+}
+
+function buttonEight() {
+  calcArray.push(8);
+  console.log(calcArray);
+}
+
+function buttonNine() {
+  calcArray.push(9);
+  console.log(calcArray);
+}
+
+function buttonPlus() {
+  calcArray.push("+");
+  console.log(calcArray);
+}
+
+function buttonMinus() {
+  calcArray.push("-");
+  console.log(calcArray);
+}
+
+function buttonDividedBy() {
+  calcArray.push("/");
+  console.log(calcArray);
+}
+
+function buttonMultiplication() {
+  calcArray.push("*");
+  console.log(calcArray);
+}
+
+function buttonDecimalPoint() {
+  calcArray.push(".");
+  console.log(calcArray);
+}
