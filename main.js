807928174de24d39e58228ddf686ef54cabdf5f6
Zach Morse <zacharymorse@gmail.com>
2017-06-13 23:18:42 -0500

    small css change, JS refactoring and conditional checking

--- main.js
+++ main.js
@@ -1,17 +1,7 @@
-//
-// This program will:
-// 1. listen for keypresses of each element
-// 2. display each keypress in display panel
-// 3. functions will concat to string when pressed
-// 4. string is concatenated with each keypress
-// 5. string elements will be joined together
-// 6. string will be solved as equation by eval when "equals" is pressed
-// 7. "C" button will convert string back to empty
-
 // Declaring initial global variables:
 
-var calcString = ""; // initial empty string for calculation
-var nums = document.querySelectorAll(".content"); // gets an array of all the number and operator buttons
+var calcString = "";
+var nums = document.querySelectorAll(".content");
 var buttonClr = document.getElementById("button-clr");
 var buttonEquals = document.getElementById("button-equals");
 
@@ -21,21 +11,13 @@ buttonEquals.addEventListener("click", buttonEvaluate);
 // function calls for each button:
 
 function concatNum(event) {
-  console.log(event);
-  console.log(event.target);
   var value = event.target.textContent;
-  console.log("value: ", value);
   calcString += value;
-  console.log(calcString);
   displayUpdate();
 }
 
 function displayUpdate() {
   document.getElementById("display").innerHTML = calcString;
-}
-
-for (var i = 0; i < nums.length; i++) {
-  nums[i].addEventListener("click", concatNum);
 }
 
 function buttonClear() {
@@ -44,6 +26,22 @@ function buttonClear() {
 }
 
 function buttonEvaluate() {
-  console.log(eval(calcString));
-  buttonClear();
+  var typeChecker = calcString.charAt(calcString.length - 1);
+
+  if (
+    typeChecker == "/" ||
+    typeChecker == "*" ||
+    typeChecker == "+" ||
+    typeChecker == "-"
+  ) {
+    document.getElementById("display").innerHTML = "Invalid";
+    setTimeout(buttonClear, 500);
+  } else {
+    calcString = eval(calcString);
+    displayUpdate();
+  }
+}
+
+for (var i = 0; i < nums.length; i++) {
+  nums[i].addEventListener("click", concatNum);
 }
